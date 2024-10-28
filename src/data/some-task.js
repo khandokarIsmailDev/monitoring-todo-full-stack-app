e64b4e2004f13ff34718b07c416a65b17cdff9ed
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-10-28 17:48:43 +0600

    create some demo task

---
+++ src/data/some-task.js
@@ -0,0 +1,26 @@
+export const someTask = [
+    {
+        taskName: "this is a task one",
+        description: "this is a description for task one",
+        dueDate: new Date(),
+        category: "todo"
+    },
+    {
+        taskName: "this is a task two",
+        description: "this is a description for task two",
+        dueDate: new Date(),
+        category: "inprogress"
+    },
+    {
+        taskName: "this is a task three",
+        description: "this is a description for task three",
+        dueDate: new Date(),
+        category: "done"
+    },
+    {
+        taskName: "this is a task four",
+        description: "this is a description for task four",
+        dueDate: new Date(),
+        category: "revised"
+    }
+]
