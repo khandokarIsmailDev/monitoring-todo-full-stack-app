b1aa69e940200ba118ba862fee9a895b047b5f74
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-10-28 23:53:48 +0600

    fix error

--- src/components/todos/TodoBoard.jsx
+++ src/components/todos/TodoBoard.jsx
@@ -1,38 +1,50 @@
-import React, { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import TodoAdd from "./TodoAdd";
 import Todo from "./Todo";
 import OnProgress from "./OnProgress";
 import Done from "./Done";
 import Revised from "./Revised";
 import { TodoContext } from "../../context";
-import { someTask } from "../../data/some-task";
 
 export default function TodoBoard() {
-  const { todo,setTodo } = useContext(TodoContext);
+  const { todoAll, setTodoAll } = useContext(TodoContext);
+  const [todoList, setTodoList] = useState({
+    todo: [],
+    inprogress: [],
+    done: [],
+    revised: [],
+  });
 
   useEffect(() => {
-    setTodo(someTask);
-  }, []);
+    const categoriyTask = {
+      todo: [],
+      inprogress: [],
+      done: [],
+      revised: [],
+    };
 
-  console.log(todo);
+    todoAll.forEach((task) => {
+      for (const category of Object.keys(categoriyTask)) {
+        if (task.category === category) {
+          categoriyTask[category].push(task);
+        }
+      }
+    });
 
+    setTodoList(categoriyTask);
+  }, [todoAll]);
+
+  console.log("this is todoList", todoList);
+  console.log("this is todoAll for context", todoAll);
   return (
     <div className="mx-auto max-w-7xl p-6">
       <TodoAdd />
       <div className="-mx-2 mb-6 flex flex-wrap">
-        {
-          todo.map((task) => (
-            <>
-              {task.category === "todo" && <Todo task={task} />}
-              {task.category === "inprogress" && <OnProgress task={task} />}
-              {task.category === "done" && <Done task={task} />}
-              {task.category === "revised" && <Revised task={task} />}
-            </>
-          ))
-        }
-
+        <Todo tasks={todoList.todo} />
+        <OnProgress tasks ={todoList.inprogress} />
+        <Done tasks={todoList.done} />
+        <Revised tasks={todoList.revised} />
       </div>
     </div>
   );
 }
-  
--- src/data/some-task.js
+++ src/data/some-task.js
@@ -1,26 +1,37 @@
 export const someTask = [
     {
+        id: 1,
         taskName: "this is a task one",
         description: "this is a description for task one",
-        dueDate: new Date(),
+        dueDate: "February 20, 2024",
         category: "todo"
     },
     {
+        id: 2,
         taskName: "this is a task two",
         description: "this is a description for task two",
-        dueDate: new Date(),
+        dueDate: "February 21, 2024",
         category: "inprogress"
     },
     {
+        id: 3,
         taskName: "this is a task three",
         description: "this is a description for task three",
-        dueDate: new Date(),
+        dueDate: "February 30, 2024",
         category: "done"
     },
     {
+        id: 4,
         taskName: "this is a task four",
         description: "this is a description for task four",
-        dueDate: new Date(),
+        dueDate: "February 31, 2024",
         category: "revised"
+    },
+    {
+        id: 5,
+        taskName: "this is task five",
+        description: "this is a description for task five",
+        dueDate: "March 01, 2024",
+        category: "todo"
     }
 ]
