6b0284ee0643b8e4556bfe2bcc05367a1e2724df
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-10-28 17:49:03 +0600

    add demo task in main state

--- src/components/todos/TodoBoard.jsx
+++ src/components/todos/TodoBoard.jsx
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import TodoAdd from "./TodoAdd";
 import Todo from "./Todo";
 import OnProgress from "./OnProgress";
 import Done from "./Done";
 import Revised from "./Revised";
+import { TodoContext } from "../../context";
+import { someTask } from "../../data/some-task";
 
 export default function TodoBoard() {
+  const { todo,setTodo } = useContext(TodoContext);
+
+  useEffect(() => {
+    setTodo(someTask);
+  }, []);
+
+  console.log(todo);
+
   return (
     <div className="mx-auto max-w-7xl p-6">
       <TodoAdd />
       <div className="-mx-2 mb-6 flex flex-wrap">
-        <Todo />
-        <OnProgress />
-        <Done />
-        <Revised />
+        {
+          todo.map((task) => (
+            <>
+              {task.category === "todo" && <Todo task={task} />}
+              {task.category === "inprogress" && <OnProgress task={task} />}
+              {task.category === "done" && <Done task={task} />}
+              {task.category === "revised" && <Revised task={task} />}
+            </>
+          ))
+        }
+
       </div>
     </div>
   );
 }
+  
