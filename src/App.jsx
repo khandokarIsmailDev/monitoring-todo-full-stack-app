955b2a8b1a1019a38c3250a45a50f750201d426f
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-10-28 23:54:18 +0600

    change the context value

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React, { useContext ,useState} from "react";
 import Page from "./Page";
 import { TodoContext } from "./context";
+import { someTask } from "./data/some-task";
 
 export default function App() {
 
-  const [todo,setTodo] = useState([])
+  const [todoAll,setTodoAll] = useState(someTask)
+
 
   return (
     <div>
-      <TodoContext.Provider value={{ todo, setTodo }}>
+      <TodoContext.Provider value={{ todoAll, setTodoAll }}>
         <Page />
       </TodoContext.Provider>
     </div>
